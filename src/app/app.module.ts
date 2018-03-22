a44dfb8f044d1c0895316a8b5d4aa7d2fb238bc0
Lanchon lLudovic <ludovic.lanchon@gmail.com>
2018-03-22 13:42:22 +0100

    add firebase and angularfire to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBPcNBKJr3PtrgE92Txxe3woaEc75CpsNI",
+    authDomain: "gomoku-projet-2.firebaseapp.com",
+    databaseURL: "https://gomoku-projet-2.firebaseio.com",
+    projectId: "gomoku-projet-2",
+    storageBucket: "gomoku-projet-2.appspot.com",
+    messagingSenderId: "1001534234475"
+  }
 };
+
