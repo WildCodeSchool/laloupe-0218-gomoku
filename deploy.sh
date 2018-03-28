fbad827144d1401bbffcfe098db5522fe1a9e2fc
martin donadieu <martindonadieu@gmail.com>
2018-03-28 21:21:40 +0200

    delete verbose

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ owner="$(cut -d'/' -f1 <<< ${TRAVIS_REPO_SLUG})"
 repo="$(cut -d'/' -f2 <<< ${TRAVIS_REPO_SLUG})"
 
 if [ -z "${TRAVIS_PULL_REQUEST}" -o "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  npm run build -- --verbose --base-href "https://${owner}.github.io/${repo}/" -dop false
-  angular-cli-ghpages --no-silent
+  npm run build -- --base-href "https://${owner}.github.io/${repo}/" -dop false
+  angular-cli-ghpages
 else
-  npm run build -- --verbose --base-href "https://${owner}.github.io/${repo}/pr/${TRAVIS_PULL_REQUEST}/" -op "./dist/pr/${TRAVIS_PULL_REQUEST}" -dop false
-  angular-cli-ghpages --no-silent
+  npm run build -- --base-href "https://${owner}.github.io/${repo}/pr/${TRAVIS_PULL_REQUEST}/" -op "./dist/pr/${TRAVIS_PULL_REQUEST}" -dop false
+  angular-cli-ghpages
   npm run lh --score=70 https://${owner}.github.io/${repo}/pr/${TRAVIS_PULL_REQUEST}/
 fi
