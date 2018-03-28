c20506646df0757b0ef923d827a33c9ac23a9661
Lanchon lLudovic <ludovic.lanchon@gmail.com>
2018-03-28 14:41:49 +0200

    update app-routing.module : add '**' redirect + rm footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { GameComponent } from './game/game.component';
-import { FooterComponent } from './footer/footer.component';
 import { ProfilComponent } from './profil/profil.component';
 const routes: Routes = [
     { path: '', redirectTo: 'landing-page', pathMatch: 'full' },
     { path: 'landing-page', component: LandingPageComponent },
     { path: 'connexion', component: ConnexionComponent },
     { path: 'game', component: GameComponent },
-    { path: 'footer', component: FooterComponent },
     { path: 'profil', component: ProfilComponent },
+    { path: '**', redirectTo: 'landing-page', pathMatch: 'full' },
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
